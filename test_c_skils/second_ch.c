e7d43fd31ec4b32590aa68ee3fd7557d90e36b93
Maitri Panchal <maitripanchal1408@gmail.com>
2024-12-23 12:07:27 +0530

    initial commit

--- test_c_skils/second_ch.c
+++ test_c_skils/second_ch.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
 int main()
-{
-  int i = 1;
-  switch (i)
-  {
-  case 1:
-    printf("hello");
-    break;
-  case 1 * 2 + 4:
-    printf("world");
-    break;
-  }
-  return 0;
+{ // question 6
+  // int i = 1;
+  // switch (i)
+  // {
+  // case 1:
+  //   printf("hello");
+  //   break;
+  // case 1 * 2 + 4:
+  //   printf("world");
+  //   break;
+  // }
+
+  // question 7
+  // int a = 10;
+  // switch (a)
+  // {
+  // }
+  // printf("hello");
+
+  // question 8
+  // int i = 1;
+  // switch (i)
+  // {
+  //   printf("default");
+  // case 1:
+  //   printf("case1");
+  //   break;
+  // case 2:
+  //   printf("case2");
+  //   break;
+  // }
+
+  // question 9
+  // int a = 1, b;
+  // b = a < 10 ? 20 : b;
+  // printf("%d", b);
+
+  // question 10
+  // int a = 10, b;
+  // a >= 5 ? b = 100 : (b = 200);
+  // printf("%d", b);
+
+    return 0;
 }
