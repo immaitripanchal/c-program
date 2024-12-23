18fa41ec2a8ed121f39859454e7d93dfb138a70d
Maitri Panchal <maitripanchal1408@gmail.com>
2024-12-23 10:47:00 +0530

    initial commit

--- test_c_skils/second_ch.c
+++ test_c_skils/second_ch.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main()
 {
+  int i = 1;
+  switch (i)
+  {
+  case 1:
+    printf("hello");
+    break;
+  case 1 * 2 + 4:
+    printf("world");
+    break;
+  }
   return 0;
 }
