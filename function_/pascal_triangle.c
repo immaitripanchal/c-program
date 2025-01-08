f5fe528b6cab61c643f0e160dc1e42488fea7264
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-08 21:03:08 +0530

    initial commit

--- function_/pascal_triangle.c
+++ function_/pascal_triangle.c
@@ -6,14 +6,15 @@ int main()
   int n;
   printf("enter n :");
   scanf("%d", &n);
-  for (int i = 0; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
-    for(int k=0; k<=n-i+1; k++){
+    for (int k = 0; k < n - i + 1; k++)
+    {
       printf(" ");
     }
     for (int j = 0; j <= i; j++)
     {
-      int icj = combination(i,j);
+      int icj = combination(i, j);
       printf("%d ", icj);
     }
     printf("\n");
